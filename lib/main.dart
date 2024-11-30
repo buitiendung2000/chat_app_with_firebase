fd0c1361cfe5e3a212c7a1a3afea9d97f0f07401
buitiendung <dung60th1@gmail.com>
2024-11-30 20:00:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:chat_app/chat.dart';
+import 'package:chat_app/firebase_options.dart';
+import 'package:chat_app/splash.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'auth.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Chat',
+      theme: ThemeData().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 63, 17, 177))),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+          if (snapshot.hasData) {
+            return ChatScreen();
+          }
+          return const AuthScreen();
+        },
+      ),
+    );
+  }
+}
